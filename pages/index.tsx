c094372f93680a0f27c08611ff55fb3ddf6b535a
Eusoff <eusoffamir@gmail.com>
2026-05-16 21:07:39 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -303,7 +303,7 @@ export default function Home() {
                 </div>
               </div>
 
-              <a href={result.downloadUrl} download="highlighted.pdf" className="btn-download">
+              <a href={result.downloadUrl} target="_blank" rel="noreferrer" className="btn-download">
                 <svg viewBox="0 0 24 24">
                   <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/>
                 </svg>
